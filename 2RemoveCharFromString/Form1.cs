0234573d4a5af020bd53e03ecdcf22156333d689
ArtyTheTorch <artysaligator@yahoo.com>
2014-10-19 11:16:47 -0700

    Removes a Character from a string
    Also implemented an UI interface

---
+++ 2RemoveCharFromString/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2RemoveCharFromString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char[] c = textBox1.Text.ToCharArray();
+            char[] str = textBox2.Text.ToCharArray();
+
+            RemoveCharFromString r = new RemoveCharFromString();
+
+            label3.Text = r.remove(c,str);
+        }
+    }
+}
---
+++ 2RemoveCharFromString/RemoveCharFromString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2RemoveCharFromString
+{
+    class RemoveCharFromString
+    {
+
+        internal string remove(char[] c, char[] str)
+        {
+            string results = "";
+            foreach (char i in c) {
+                if (i != str[0])
+                { 
+                    results += i;
+                }
+            }
+            return results;
+        }
+    }
+}
